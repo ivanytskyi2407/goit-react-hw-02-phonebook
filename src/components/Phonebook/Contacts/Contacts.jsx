12bad2a41a4c3186696e53a0fe0f48b0ad4b0172
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-05-03 16:56:40 +0300

    Task 1-2

---
+++ src/components/Phonebook/Contacts/Contacts.jsx
@@ -0,0 +1,17 @@
+// import s from './Contacts.module.css';
+
+const Contacts = ({ contacts }) => {
+  return (
+    <div>
+      <h2>Contacts</h2>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id}>
+            {contact.name}: {contact.number}
+          </li>
+        );
+      })}
+    </div>
+  );
+};
+export default Contacts;
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,38 @@
+import s from './Phonebook.module.css';
+
+const Phonebook = () => {
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form className={s.form}>
+        <label className={s.label} for="name">
+          Name
+        </label>
+        <input
+          className={s.input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <label className={s.label} for="number">
+          Number
+        </label>
+        <input
+          className={s.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <button className={s.button} type="submit">
+          Add Contact
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Phonebook;
